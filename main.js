baf7d06d82796b6779ac8742d0ee958a8b06c676
Men328 <22010328@st.phenikaa-uni.edu.vn>
2025-04-28 16:58:33 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // Thêm smooth scroll cho link nav
 document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
-  
+});
